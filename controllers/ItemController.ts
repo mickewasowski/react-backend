48f55dafcd9cd394852b5951bfca29be1e9422b3
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2024-01-24 21:02:20 +0200

    implemented pagination for the items

--- controllers/ItemController.ts
+++ controllers/ItemController.ts
@@ -4,43 +4,49 @@ import Item from '../models/Item';
 import { verifyToken } from '../utils/token';
 
 // @Desc Get all itemss
-// @Route /api/item
+// @Route /api/item?page=1&limit=10
 // @Method GET
 export const getAllItems = asyncHandler(async (req: Request, res: Response) => {
+    if (typeof req.query.page === 'string' && typeof req.query.limit === 'string') {
+        const page: number = parseInt(req.query.page);
+        const limit: number = parseInt(req.query.limit);
+        const startIndex = (page - 1) * limit;
 
-    const items = await Item.find({}).select(['-__v', '-owner'])
-    res.status(201).json({ success: true, count: items.length, items })
+        const items = await Item.find({}).skip(startIndex).limit(limit).select(['-__v', '-owner']);
 
-})
+        res.status(201).json({ success: true, count: items.length, items })
+    } else {
+        res.status(400).json({ success: false, message: 'Page and Limit must be provided!' });
+    }
+});
 
 // @Desc update item 
 // @Route /api/item
 // @Method PATCH
 export const updateItem = asyncHandler (async (req: Request, res: Response) => {
 
-    const newItem = req.body
-    const token = req.headers.authorization?.replace('Bearer ', '') || ''    
+    const newItem = req.body;
+    const token = req.headers.authorization?.replace('Bearer ', '') || '';
     
-    const {success, payload} = verifyToken(token)
+    const {success, payload} = verifyToken(token);
 
     if(!success || !payload) {
-        res.status(500).json({success, message: "Unauthorized"})
-        return
+        res.status(500).json({success, message: "Unauthorized"});
+        return;
     }
 
-    const item = await getMutableItem(newItem._id, payload.id, res)
-    if (!item) return
+    const item = await getMutableItem(newItem._id, payload.id, res);
+    if (!item) return;
     
-    item.name = newItem.name ?? item.name
-    item.description = newItem.description ?? item.description
-    item.type = newItem.type ?? item.type
-    item.image = newItem.image ?? item.image
+    item.name = newItem.name ?? item.name;
+    item.description = newItem.description ?? item.description;
+    item.type = newItem.type ?? item.type;
+    item.image = newItem.image ?? item.image;
 
-    await item.save()
+    await item.save();
 
-    res.status(201).json({ success: true, item})
-
-})
+    res.status(201).json({ success: true, item});
+});
 
 // @Desc add new item
 // @Route /api/item
@@ -48,23 +54,23 @@ export const updateItem = asyncHandler (async (req: Request, res: Response) => {
 export const postItem = asyncHandler(async (req: Request, res: Response) => {
 
     const { name, description, type, image } = req.body;
-    const token = req.headers.authorization?.replace('Bearer ', '') || ''
-    
+    const token = req.headers.authorization?.replace('Bearer ', '') || '';
+
     if(!token) {
-        res.status(401).json({success: false, message: "Token missing"})
-        return
+        res.status(401).json({success: false, message: "Token missing"});
+        return;
     }   
 
-    const {success, payload} = verifyToken(token)
+    const {success, payload} = verifyToken(token);
 
     if(!success || !payload) {
-        res.status(500).json({success, message: "Unauthorized"})
-        return
+        res.status(500).json({success, message: "Unauthorized"});
+        return;
     }    
 
     if(!name) {
-        res.status(402).json({success: false, message: "missing 'name' field"})
-        return
+        res.status(402).json({success: false, message: "missing 'name' field"});
+        return;
     }
     
     const item = new Item({
@@ -73,51 +79,49 @@ export const postItem = asyncHandler(async (req: Request, res: Response) => {
 
     await item.save();
 
-    res.status(201).json({ success: true, item})
-
-})
+    res.status(201).json({ success: true, item});
+});
 
 // @Desc delete item
 // @Route /api/item
 // @Method DELETE
 export const deleteItem = asyncHandler(async (req: Request, res: Response) => {
 
-    const { _id } = req.body
-    const token = req.headers.authorization?.replace('Bearer ', '') || ''    
+    const { _id } = req.body;
+    const token = req.headers.authorization?.replace('Bearer ', '') || '';
     
-    const {success, payload} = verifyToken(token)
+    const {success, payload} = verifyToken(token);
 
     if(!success || !payload) {
-        res.status(500).json({success, message: "Unauthorized"})
-        return
+        res.status(500).json({success, message: "Unauthorized"});
+        return;
     } 
 
-    const item = await getMutableItem(_id, payload.id, res)
-    if (!item) return
+    const item = await getMutableItem(_id, payload.id, res);
+    if (!item) return;
 
-    await item.remove()
+    await item.remove();
     
-    res.status(201).json({ success: true, message: "deleted"})
-
-})
+    res.status(201).json({ success: true, message: "deleted"});
+});
 
 const getMutableItem = async (itemId: string, ownerId: string, res: Response) => {
     if(!itemId) {
-        res.status(401).json({success: true, message: "Item's '_id' is required"})
-        return
+        res.status(401).json({success: true, message: "Item's '_id' is required"});
+        return;
     }
 
-    const item = await Item.findOne({ _id: itemId })
+    const item = await Item.findOne({ _id: itemId });
     
     if(!item) {
-        res.status(402).json({ success: false, message: "Item not found"})
-        return
+        res.status(402).json({ success: false, message: "Item not found"});
+        return;
     }
 
     if(!item.hasSameOwner(ownerId)){
-        res.status(403).json({ success: false, message: "Only the owner of an item can update the item"})
-        return
+        res.status(403).json({ success: false, message: "Only the owner of an item can update the item"});
+        return;
     }
 
-    return item
-}
+    return item;
+};
